62001c8cc8d74ab251ae806ffc4ac9561c1b3870
harinij <harinijanakiraman@gmail.com>
2016-04-13 20:19:25 +0800

    Brand New Cards with Videos

--- js/index.js
+++ js/index.js
@@ -113,7 +113,24 @@ by Pierre Bonnin - @PierreBonninPRO - 2015
 })(jQuery);
 
 
+autoPlayYouTubeModal();
 
+  //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+  function autoPlayYouTubeModal() {
+      var trigger = $("body").find('[data-toggle="modal"]');
+      trigger.click(function () {
+          var theModal = $(this).data("target"),
+              videoSRC = $(this).attr("data-theVideo"),
+              videoSRCauto = videoSRC + "?autoplay=1";
+          $(theModal + ' iframe').attr('src', videoSRCauto);
+          $(theModal + ' button.close').click(function () {
+              $(theModal + ' iframe').attr('src', videoSRC);
+          });
+          $('.modal').click(function () {
+              $(theModal + ' iframe').attr('src', videoSRC);
+          });
+      });
+  }
 
 function onChange(){
   console.log('cols number changed');
